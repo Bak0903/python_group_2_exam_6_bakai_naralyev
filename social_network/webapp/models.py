472390e454b772a6b2b1b6cf5c4e44f403048244
bak0903 <bak0903@mail.ru>
2019-01-12 12:30:40 +0600

    Added Userinfo and Post models

--- social_network/webapp/models.py
+++ social_network/webapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Userinfo(models.Model):
+    user = models.OneToOneField(User, on_delete=models.PROTECT, related_name='client', verbose_name='Пользователь')
+    phone = models.CharField(max_length=50, verbose_name='Телефон')
+    friends = models.ManyToManyField(User, related_name='clients_friend', verbose_name='друзья')
+
+    def __str__(self):
+        return self.user.get_full_name()
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, null=False, blank=False, verbose_name='Заголовок')
+    text = models.TextField(max_length=3000, null=False, blank=False, verbose_name='Текст')
+    author = models.CharField(max_length=40, null=True, blank=True, verbose_name='Автор')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+
+    def __str__(self):
+        return self.title
+
+
