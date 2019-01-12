3a9fc796b23c4c9cfb41bc7e3228ca66db784083
bak0903 <bak0903@mail.ru>
2019-01-12 12:31:21 +0600

    Config admin.py

--- social_network/webapp/admin.py
+++ social_network/webapp/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+from webapp.models import Userinfo, Post
 
-# Register your models here.
+
+class UserinfoInline(admin.StackedInline):
+    model = Userinfo
+
+
+class UserinfoAdmin(UserAdmin):
+    inlines = [UserinfoInline]
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserinfoAdmin)
+admin.site.register(Post)
