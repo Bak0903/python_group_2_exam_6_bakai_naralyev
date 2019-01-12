954d9627447e80333a3b2a2dee7a21fc042a2a5a
bak0903 <bak0903@mail.ru>
2019-01-12 19:22:59 +0600

    Urls fix

--- social_network/webapp/urls.py
+++ social_network/webapp/urls.py
@@ -6,7 +6,7 @@ from webapp.views import PostListView, PostDetailView, PostCreateView, PostUpdat
 app_name = 'webapp'
 
 urlpatterns = [
-    path('post_list/', PostListView.as_view(), name='post_list'),
+    path('', PostListView.as_view(), name='post_list'),
     path('post/create', PostCreateView.as_view(), name='post_create'),
     path('post/<int:pk>', PostDetailView.as_view(), name = 'post_detail'),
     path('post/<int:pk>/update', PostUpdateView.as_view(), name='post_update'),
